e0e0bf9e63f2cd904ffcd4a413cf417e91dd5563
carlo <carlo@desktop.station>
2018-12-03 22:24:54 +0100

    updated shop link and logo

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,39 +3,35 @@ import { Link } from 'gatsby'
 import logo from '../img/logo.png'
 import Dropdown from '../components/Dropdown'
 
+const links = () => (
+  <div className="navbar-links">
+    <a
+      className="navbar-item"
+      href="https://www.etsy.com/it/shop/UfficioMisteri"
+      target="_blank"
+      rel="noopener noreferrer"
+    >
+      <h4 className="title is-4">Shop</h4>
+    </a>
+    <Link className="navbar-item" to="/about">
+      <h4 className="title is-4">About</h4>
+    </Link>
+  </div>
+)
+
 const Navbar = () => (
   <nav className="navbar is-transparent">
     <div className="container">
       <div className="navbar-brand">
         <Dropdown>
-          <a
-            className="navbar-item"
-            href="https://altairiv.bigcartel.com/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h4 className="title is-4">Shop</h4>
-          </a>
-          <Link className="navbar-item" to="/about">
-            <h4 className="title is-4">About</h4>
-          </Link>
+          {links()}
         </Dropdown>
         <Link to="/" className="logo">
           <img src={logo} alt="Ufficio Misteri Logo" />
         </Link>
       </div>
       <div className="navbar-end">
-        <a
-          className="navbar-item"
-          href="https://altairiv.bigcartel.com/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h4 className="title is-4">Shop</h4>
-        </a>
-        <Link className="navbar-item" to="/about">
-          <h4 className="title is-4">About</h4>
-        </Link>
+        {links()}
       </div>
     </div>
   </nav>
