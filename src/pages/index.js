d1a7bf46f8ecb536181030ed5cde5137f7f2a129
carlo <carlo@desktop.station>
2018-12-17 20:26:00 +0100

    Fixed bug on sort

--- src/pages/index.js
+++ src/pages/index.js
@@ -58,7 +58,6 @@ IndexPage.propTypes = {
 export const pageQuery = graphql`
   query IndexQuery {
     allMarkdownRemark(
-      sort: { order: DESC, fields: [frontmatter___date] },
       filter: { frontmatter: { templateKey: { eq: "blog-post" } }}
     ) {
       edges {
